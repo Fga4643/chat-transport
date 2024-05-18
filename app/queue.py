99709134a09a93dec736a68cea24056a1ea74b01
Fga4643 <obuhov.fga4643@yandex.ru>
2024-05-18 13:50:18 +0300

    main

---
+++ app/queue.py
@@ -0,0 +1,34 @@
+from json import dumps
+
+from kafka import KafkaProducer, KafkaConsumer
+
+
+def sendKafka(message, key="testnum"):
+    print("sendKafka")
+    print(message, key)
+    my_producer = KafkaProducer(
+        bootstrap_servers=['localhost:29092'],
+        value_serializer=lambda x: dumps(x).encode('utf-8')
+    )
+
+    my_producer.send(key, value=message)
+
+
+def getKafka(key="testnum"):
+    print("getKafka")
+    consumer = KafkaConsumer(key,
+                             bootstrap_servers=['localhost:29092'],
+                             group_id='test',
+                             auto_offset_reset='earliest')
+
+    res_str = []
+
+    message = consumer.poll(0.1)
+
+    for records in message.values():
+        for msg in records:
+            res_str.append(msg.value.decode("utf-8").strip('"'))
+
+    return res_str
+
+
