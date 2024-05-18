9180d1776607b678492b6ca94d3b17ad3dca776c
Fga4643 <obuhov.fga4643@yandex.ru>
2024-05-18 20:22:56 +0300

    main

--- transport/settings.py
+++ transport/settings.py
@@ -19,8 +19,6 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    "channels",
-    "daphne",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
